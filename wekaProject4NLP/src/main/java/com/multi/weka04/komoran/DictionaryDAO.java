0d6a51be5c59cfe7ec61e6adf5587a8d3863cfcd
DESKTOP-MMRE54H\dgony <98igunhi@naver.com>
2023-12-07 17:43:53 +0900

    12월 7일 실습내용 myBatis 복습과 프로젝트 테이블 정하기

--- wekaProject4NLP/src/main/java/com/multi/weka04/komoran/DictionaryDAO.java
+++ wekaProject4NLP/src/main/java/com/multi/weka04/komoran/DictionaryDAO.java
@@ -13,6 +13,7 @@ public interface DictionaryDAO {
 	MongoTemplate mongo;
 	
 	public void one(String word); {
+		// where  word = '최고
 		Query query = new Query(new Criteria("word").is(word));
 		mongo.findOne(query, DictionaryVO.class, "dictionary");
 	}
--- wekaProject4NLP/src/main/java/com/multi/weka04/komoran/DictionaryService.java
+++ wekaProject4NLP/src/main/java/com/multi/weka04/komoran/DictionaryService.java
@@ -1,22 +1,39 @@
-package com.multi.weka04.komoran;
+package com.multi.mongo;
 
+import java.util.Iterator;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
 import kr.co.shineware.nlp.komoran.core.Komoran;
 import kr.co.shineware.nlp.komoran.model.KomoranResult;
 
-@Service // controller -- service(전처리 / 후처리) -- dao
-public interface DictionaryService {
+@Service //controller --- service(전처리/후처리) --- dao 
+public class DictionaryService {
+
+	@Autowired
+	DictionaryDAO dao;
 	
-	public void emotion (String sentence) {
-		// komoran 이용해서 명사만 추출 후 프린트
+	public String emotion(String sentence) {
+		//코모란 이용해서 명사만 추출후 프린트 
 		Komoran komoran = new Komoran(DEFAULT_MODEL.FULL);
-		KomoranResult result = komoran.analyze(setence);
-		List<String> list = result.getNouns();
-		System.out.println(listm);
+		KomoranResult result = komoran.analyze(sentence);
+		List<String> list = result.getNouns(); //[유재석, 최고, 재미, !!]
+		System.out.println(list);
+		int sum = 0;
+		for (String s : list) {
+			DictionaryVO vo = dao.one(s);
+			if(vo != null) {
+				System.out.println(vo.getJumsu());
+				sum = sum + vo.getJumsu(); //sum += vo.getJumsu();
+			}//if
+		}//for
+		String result2 = "negative";
+		if(sum >= 0) {
+			result2 = "positive";
+		}
+		return result2;
 	}
-	
 }
